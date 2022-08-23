69f4132268e6c26c25cb50e1d3aeda5839b3a978
Shahbaz <shahbaz.hassan@digitalux.pk>
2022-08-23 12:46:57 +0500

    added fontawesome and tailwind setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router,Route, Routes} from "react-router-dom";
 import Home from "./components/Home/Home";
 
+
 function App() {
     return (
         <Router>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "@fontsource/montserrat";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
