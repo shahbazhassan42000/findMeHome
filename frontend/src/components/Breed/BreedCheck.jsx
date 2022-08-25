8905737dbb44efa921b74703ce6128ce359164e3
Shahbaz <shahbaz.hassan@digitalux.pk>
2022-08-25 23:58:54 +0500

    added upload to imgBB api

--- frontend/src/components/Breed/BreedCheck.jsx
+++ frontend/src/components/Breed/BreedCheck.jsx
@@ -1,7 +1,14 @@
 import logo from "../../assets/images/find_me_home_logo.png"
 import dog from "../../assets/icons/dog.png"
+import {useRef, useState} from "react";
+import axios from "axios";
+import Loading from "../Loading";
 
 const BreedCheck = () => {
+    const [fileName, setFileName] = useState({name: "Upload Picture of Dog"});
+    const [msg, setMsg] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const imgUpload = useRef(null);
     return (
         <div className="flex flex-col w-[50%] mx-auto items-center">
             <div className="w-[220px] mt-[20px]">
@@ -15,34 +22,86 @@ const BreedCheck = () => {
                     <p className="text-[#5A8081] mt-[15px] px-[40px] text-center leading-[16px]">
                         Please write the name and add the picture of Dog to find out its Breed
                     </p>
-                    <form className="flex flex-col mt-[25px] w-full px-[33px] ml-1 justify-center items-center">
-                        <label className="border border-[#7F99A2] active:border-[#A7B4BF] hover:border-[#5A8081] active:bg-[#5A8081] active:text-white px-[12px] py-[4px] rounded-[7px] w-full flex items-center mb-5">
-                            <div className="w-[28px] mr-[10px]"><img src={dog} alt="dog icon" /></div>
-                            <input className="w-full bg-transparent outline-0 placeholder:text-[#7F99A2] active:placeholder:text-white hover:placeholder:text-[#5A8081]" type="text" placeholder="Dog Name"/>
+                    <form
+                        onSubmit={(e) => onFormSubmit(e, setMsg, setLoading, setFileName)}
+                        className="flex flex-col mt-[25px] w-full px-[33px] ml-1 justify-center items-center">
+                        <label
+                            className="border border-[#7F99A2] active:border-[#A7B4BF] hover:border-[#5A8081] active:bg-[#5A8081] active:text-white px-[12px] py-[4px] rounded-[7px] w-full flex items-center mb-5">
+                            <div className="w-[28px] mr-[10px]"><img src={dog} alt="dog icon"/></div>
+                            <input
+                                className="w-full bg-transparent outline-0 placeholder:text-[#7F99A2] active:placeholder:text-white hover:placeholder:text-[#5A8081]"
+                                type="text" name="dogName" placeholder="Dog Name" required/>
                         </label>
-                        <button className="border border-[#7F99A2] hover:border-[#5A8081] px-[12px] py-[4px] rounded-[7px] w-full flex justify-between items-center text-[#7F99A2] hover:text-[#5A8081]">
-                            <div className="flex items-center">
+                        <input
+                            ref={imgUpload}
+                            onChange={(e) => {
+                                setFileName(e.target.files[0] ?? {name: "Upload Picture of Dog"})
+                            }}
+                            className="hidden absolute left-0 py-[1px] z-10" name="image"
+                            type="file" accept="image/jpeg, image/jpg, image/png" required
+                        />
+                        <button
+                            onClick={(e) => onFileChange(e, imgUpload.current)}
+                            className=" relative border border-[#7F99A2] hover:border-[#5A8081] px-[12px] py-[4px] rounded-[7px] w-full flex justify-between items-center text-[#7F99A2] hover:text-[#5A8081]">
+                            <div className="flex items-center text-ellipsis overflow-hidden whitespace-nowrap">
                                 <span className="fa-regular fa-image mr-[10px] text-[24.4px]"></span>
-                                Upload Picture of Dog
+                                {fileName.name}
                             </div>
-                            <span className="fa fa-arrow-up-from-bracket text-[24.4px]"></span>
+                            <span className="ml-[10px] fa fa-arrow-up-from-bracket text-[24.4px]"></span>
                         </button>
-                        <button className="mt-[25px] px-[12px] py-[4px] rounded-[7px] w-full flex items-center bg-[#E0DFE2] hover:bg-[#5A8081] text-[#3E665C] hover:text-white font-bold flex justify-center">
+                        <p style={{color: msg === "ERROR! while uploading, Please try again" ? '#ff0033' : '#567a0d'}}
+                           className="mt-3 -mb-3 text-center">
+                            {msg}
+                        </p>
+                        <button
+                            disabled={!(fileName && fileName.name !== "Upload Picture of Dog" && fileName.name !== "")}
+                            className="mt-[25px] px-[12px] py-[4px] rounded-[7px] w-full flex items-center bg-[#E0DFE2] hover:bg-[#5A8081] disabled:bg-[#091e420a]  disabled:cursor-not-allowed text-[#3E665C] hover:text-white disabled:text-[#a5adba] font-bold flex justify-center">
                             <span>Check Breed</span>
                         </button>
                     </form>
                     <div className="mt-[25px] italic text-center">
                         <div>
                             <span className="text-[#7F99A2] text-[14px] mr-2">Be our member to adopt Dog</span>
-                            <a href="frontend/src/components/Breed/BreedCheck#" className="text-[#5A8081] font-bold hover:text-[#3E665C]">Register</a>
+                            <a href="/register" className="text-[#5A8081] font-bold hover:text-[#3E665C]">Register</a>
                         </div>
-                        <a href="frontend/src/components/Breed/BreedCheck#" className="text-[#5A8081] font-bold hover:text-[#3E665C]">Login</a>
+                        <a href="/login" className="text-[#5A8081] font-bold hover:text-[#3E665C]">Login</a>
                     </div>
                 </div>
             </div>
+            {loading && <Loading/>}
         </div>
+
     );
 
 }
 
+const onFileChange = (e, imgUpload) => {
+    e.preventDefault();
+    imgUpload.click();
+}
+
+const onFormSubmit = (e, setMsg, setLoading, setFileName) => {
+    setLoading(true);
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const formData = Object.fromEntries(form.entries());
+    console.log(formData);
+    const imgBBURL = "https://api.imgbb.com/1/upload";
+    const expiry = 600;
+    const apiKey = "4d0eff80cd1cea3d5f1f524ac3a0808a";
+    axios.post(imgBBURL + "?expiration=" + expiry + "&key=" + apiKey, form)
+        .then(res => {
+            setMsg("Data uploaded successfully, now under processing.\nPlease Wait!");
+            console.log('response', res)
+            console.log('response URL', res.data.data.image.url)
+            console.log('success')
+            e.target.reset();
+            setFileName({name: "Upload Picture of Dog"});
+            setLoading(false);
+        }).catch(err => {
+        console.log("ERROR: ", err);
+        setMsg = ("ERROR! while uploading, Please try again");
+        setLoading(false);
+    })
+}
 export default BreedCheck;
--- frontend/src/components/Breed/BreedResult.jsx
+++ frontend/src/components/Breed/BreedResult.jsx
@@ -1,10 +1,13 @@
 import logo from "../../assets/images/find_me_home_logo.png";
 import dog from "../../assets/images/dummy_dog.png"
 import {useRef} from "react";
-import html2pdf from "html2pdf.js";
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
+import ap2  from "api2pdf";
+
 
 const BreedResult = () => {
-    const breedData=useRef(null);
+    const breedData = useRef(null);
     return (
         <div className="flex flex-col w-[70%] mx-auto items-center">
             <div className="w-[220px] mt-[20px]">
@@ -12,7 +15,7 @@ const BreedResult = () => {
             </div>
             <div className="font-['Montserrat'] flex w-full space-x-14 items-start">
                 <div className="border-[7px] border-[#3E665C] w-[330px] h-[400px]">
-                    <img src={"https://i.ibb.co/2cxXjJf/dummy-dog.png"} alt={"dog"}/>
+                    <img src="https://i.ibb.co/2cxXjJf/dummy-dog.png" alt={"dog"}/>
                 </div>
                 <div
                     ref={breedData}
@@ -40,7 +43,8 @@ const BreedResult = () => {
                             <span className="text-[18px] italic text-[#7F99A2]">Download<br></br>Details?</span>
                             <span
                                 className="fa fa-arrow-up-from-bracket text-[48px] text-[#7F99A2] cursor-pointer hover:text-[#3E665C]"
-                                onClick={()=>generatePDF(breedData.current)}
+
+                                onClick={() => generatePDF(breedData.current)}
                             >
                             </span>
                         </div>
@@ -52,19 +56,73 @@ const BreedResult = () => {
             <div className="mt-[25px] italic text-center">
                 <div>
                     <span className="text-[#7F99A2] text-[14px] mr-2">Be our member to adopt Dog</span>
-                    <a href="frontend/src/components/Breed/BreedCheck#"
+                    <a href="/register"
                        className="text-[#5A8081] font-bold hover:text-[#3E665C]">Register</a>
                 </div>
-                <a href="frontend/src/components/Breed/BreedCheck#"
+                <a href="/login"
                    className="text-[#5A8081] font-bold hover:text-[#3E665C]">Login</a>
             </div>
         </div>
     )
 }
 
-const generatePDF=(breedData)=>{
+const generatePDF = (breedData) => {
     console.log("Generating pdf...");
-    html2pdf(breedData).save();
+
+    const img=breedData.parentElement.firstElementChild.firstElementChild;
+
+    fetch('https://v2018.api2pdf.com/chrome/html', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'YOUR-API-KEY' //Get your API key from https://portal.api2pdf.com
+        },
+        body: JSON.stringify({html: '<p>hello world from reactjs</p>', inlinePdf: true, fileName: 'test.pdf' })
+    }).then(res=>res.json())
+        .then(res => console.log(res.pdf));
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // context = canvas.getContext('2d');
+
+
+    // context.drawImage(base_image, 0, 0);
+    //
+    // const imgData = canvas.toDataURL('image/png');
+    // const pdf = new jsPDF();
+    // pdf.addImage(imgData, 'JPEG', 0, 0);
+    // pdf.save(`abc.pdf`);
+
+
+    // html2canvas(breedData)
+    //     .then((canvas) => {
+    //         const imgData = canvas.toDataURL('image/png');
+    //         const pdf = new jsPDF();
+    //         pdf.addImage(imgData, 'JPEG', 0, 0);
+    //         pdf.save(`abc.pdf`);
+    //     })
+
+
+    // let opt = {
+    //     margin: [10, 0, 10, 0],
+    //     filename: `document.pdf`,
+    //     image: {type: 'jpg', quality: 0.98},
+    //     html2canvas: {scale: 2, useCORS: true},
+    //     jsPDF: {unit: 'mm', format: 'letter', orientation: 'portrait'}
+    // };
+    // html2pdf(breedData).set(opt).save();
+    // html2pdfEs(breedData).set(opt).save();
 }
 
 export default BreedResult;
