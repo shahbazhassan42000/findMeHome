cbc9b177fb5a7861d71a43dbe90ece9f4b8125f5
Shahbaz <shahbaz.hassan@digitalux.pk>
2022-09-01 11:52:03 +0500

    created signup api for shelter and adopter

--- backend/findMeHome/resources/resources.py
+++ backend/findMeHome/resources/resources.py
@@ -1,11 +1,33 @@
+from backend.findMeHome.models.main import DBHandler
+from backend.findMeHome.models.shelter import Shelter
+from backend.findMeHome.models.user import User
 from flask_restful import Resource
 from flask import request, Response, jsonify
+
+db = DBHandler()
 
 
 class AdapterApi(Resource):
     def post(self):
         data = request.get_json()
-        print("POST")
-        print(data)
+        if data.get("user") is not None:
+            user = data["user"]
+            # use db to create
+            if data["user"]["type"] == "adopter":
+                adp = User(None, None, None, None, user["email"], user["username"], user["password"],None,None)
+                status, msg = db.add(adp)
+            elif data["user"]["type"] == "shelter":
+                shelter = Shelter(None, user["street"], user["city"], user["country"], user["email"], user["username"],
+                                  user["password"], None, None, None)
+                status, msg = db.add(shelter)
 
+            if data["user"]["type"] == "adopter" or data["user"]["type"] == "shelter":
+                if status:
+                    return jsonify(msg), 201
+                else:
+                    return jsonify(msg), 412
 
+            else:
+                return "Invalid User type", 412
+        else:
+            return "Invalid Data posted", 412
--- backend/findMeHome/resources/routes.py
+++ backend/findMeHome/resources/routes.py
@@ -1,8 +1,10 @@
+
 from backend.findMeHome.resources.resources import AdapterApi
 
 # baseUrl = 'http://127.0.0.1:5000'
 signUpUrl = '/api/v0.1/user'
 
 
+
 def initialize_routes(api):
-    api.add_resource(AdapterApi,   signUpUrl)
+    api.add_resource(AdapterApi,signUpUrl)
