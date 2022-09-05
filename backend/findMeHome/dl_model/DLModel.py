8cab782d44f0117a83a1c6bc4c5620fb886529c7
Shahbaz <shahbaz.hassan@digitalux.pk>
2022-09-05 22:29:58 +0500

    updated DLModel by rafay

---
+++ backend/findMeHome/dl_model/DLModel.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from PIL import Image
+import requests
+import numpy as np
+model = tf.keras.models.load_model("findMeHome/dl_model/fine_tuned.h5")
+
+
+def breedPredict(url, model):
+    class_names = ['Chihuahua', 'Japanese spaniel', 'Maltese dog', 'Pekinese', 'Shih-tzu', 'Blenheim spaniel',
+                   'Papillon', 'Toy terrier', 'Rhodesian ridgeback', 'Afghan hound', 'Basset', 'Beagle', 'Bloodhound',
+                   'Bluetick', 'Black-and-tan coonhound', 'Walker hound', 'English foxhound', 'Redbone', 'Borzoi',
+                   'Irish wolfhound', 'Italian greyhound', 'Whippet', 'Ibizan hound', 'Norwegian elkhound',
+                   'Otterhound', 'Saluki', 'Scottish deerhound', 'Weimaraner', 'Staffordshire bullterrier',
+                   'American staffordshire terrier', 'Bedlington terrier', 'Border terrier', 'Kerry blue terrier',
+                   'Irish terrier', 'Norfolk terrier', 'Norwich terrier', 'Yorkshire terrier',
+                   'Wire-haired fox terrier', 'Lakeland terrier', 'Sealyham terrier', 'Airedale', 'Cairn',
+                   'Australian terrier', 'Dandie dinmont', 'Boston bull', 'Miniature schnauzer', 'Giant schnauzer',
+                   'Standard schnauzer', 'Scotch terrier', 'Tibetan terrier', 'Silky terrier',
+                   'Soft-coated wheaten terrier', 'West highland white terrier', 'Lhasa', 'Flat-coated retriever',
+                   'Curly-coated retriever', 'Golden retriever', 'Labrador retriever', 'Chesapeake bay retriever',
+                   'German short-haired pointer', 'Vizsla', 'English setter', 'Irish setter', 'Gordon setter',
+                   'Brittany spaniel', 'Clumber', 'English springer', 'Welsh springer spaniel', 'Cocker spaniel',
+                   'Sussex spaniel', 'Irish water spaniel', 'Kuvasz', 'Schipperke', 'Groenendael', 'Malinois', 'Briard',
+                   'Kelpie', 'Komondor', 'Old english sheepdog', 'Shetland sheepdog', 'Collie', 'Border collie',
+                   'Bouvier des flandres', 'Rottweiler', 'German shepherd', 'Doberman', 'Miniature pinscher',
+                   'Greater swiss mountain dog', 'Bernese mountain dog', 'Appenzeller', 'Entlebucher', 'Boxer',
+                   'Bull mastiff', 'Tibetan mastiff', 'French bulldog', 'Great dane', 'Saint bernard', 'Eskimo dog',
+                   'Malamute', 'Siberian husky', 'Affenpinscher', 'Basenji', 'Pug', 'Leonberg', 'Newfoundland',
+                   'Great pyrenees', 'Samoyed', 'Pomeranian', 'Chow', 'Keeshond', 'Brabancon griffon', 'Pembroke',
+                   'Cardigan', 'Toy poodle', 'Miniature poodle', 'Standard poodle', 'Mexican hairless', 'Dingo',
+                   'Dhole', 'African hunting dog']
+    img =  np.array(Image.open(requests.get(url, stream=True).raw))
+    print('asdasdasdadasdasdadas')
+    img = tf.image.resize(img, [360, 360])
+    pred = model.predict(tf.expand_dims(img, axis=0),verbose=0)
+    pred = tf.argmax(tf.squeeze(pred))
+    class_name = class_names[pred]
+    return class_name
