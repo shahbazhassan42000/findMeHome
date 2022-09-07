f8d31fe5cc62fb9157f68e63dcadbd02841c8790
Wajahat43 <aliwajahat44@yahoo.com>
2022-09-07 20:05:25 +0500

    Added Signin and addDog api

--- backend/findMeHome/resources/resources.py
+++ backend/findMeHome/resources/resources.py
@@ -2,27 +2,28 @@ from backend.findMeHome.dl_model.DLModel import breedPredict, model
 from backend.findMeHome.models.main import DBHandler
 from backend.findMeHome.models.shelter import Shelter
 from backend.findMeHome.models.user import User
+from backend.findMeHome.models.dog import Dog
 from flask_restful import Resource
-from flask import request, Response, jsonify, make_response
+from flask import request, Response, jsonify, make_response, session
 
 db = DBHandler()
 
-
-class AdapterApi(Resource):
+class signUpApi(Resource):
     @staticmethod
     def post():
         data = request.get_json()
         if data.get("user") is not None:
             user = data["user"]
             # use db to create
-            if data["user"]["type"] == "adopter":
-                adp = User(None, None, None, None, user["email"], user["username"], user["password"], None, None)
+            if data["user"].get("type") == "adopter":
+                adp =User(user.get("fname"),user.get("lname"),user.get("city"),user.get("country"),user.get("email"),user.get("username"),
+                          user.get("password"),user.get("picture"),user.get("phone"))
                 status, msg = db.add(adp)
-            elif data["user"]["type"] == "shelter":
+            elif data["user"].get("type") == "shelter":
                 shelter = Shelter(None, user["street"], user["city"], user["country"], user["email"], user["username"],
                                   user["password"], None, None, None)
                 status, msg = db.add(shelter)
-            if data["user"]["type"] == "adopter" or data["user"]["type"] == "shelter":
+            if data["user"].get("type") == "adopter" or data["user"].get("type") == "shelter":
                 if status:
                     return make_response(jsonify(msg), 201)
                 else:
@@ -33,6 +34,56 @@ class AdapterApi(Resource):
         else:
             return "Invalid Data posted", 412
 
+#Recieves username and password
+#Returns Id of user if login was successfull
+#Returns faiure code otherwise
+class signInApi(Resource):
+    @staticmethod
+    def post():
+        data = request.get_json()
+
+        #if there is no data for key user in json return failure
+        if data.get("user") is None:
+            return "Invalid Data posted", 412
+        try:
+            user = data["user"]
+            if user.get("username") is None or user.get("password") is None:
+                return "Couldn't login. Please try again", 412
+            #call sign in function of db
+            status, id = db.signIn(user.get("username"), user.get("password"))
+            if status is True:
+                session["userid"] = id
+                return make_response(jsonify(id)), 200
+            else:
+                return "Couldn't login. Please try again", 412
+        except:
+            return "Couldn't login. Please try again", 412
+
+
+#user.id
+#dog.name
+#dog.age
+#dog.image
+class dogApi(Resource):
+    @staticmethod
+    def post():
+        data = request.get_json()
+        if data.get("user") is None or data.get("dog") is None:
+            return "Invalid Data posted", 412
+        if session.get(data.get("user").get("id")) is None:
+            return "Shelter not logged in", 412
+
+        #Create a dog object
+        dog = Dog(data.get("user").get("id"), data.get("dog").get("name"), data.get("dog").get("age")
+                  ,data.get("dog").get("dig"))
+        #add dog to db
+        status, message = db.add(dog)
+        #if add operation failed
+        if status is False:
+            return make_response(jsonify(message), 412)
+        #if add operation succecceded
+        return make_response(jsonify(message), 200)
+#
 
 class ModelApi(Resource):
     @staticmethod
--- backend/findMeHome/resources/routes.py
+++ backend/findMeHome/resources/routes.py
@@ -1,10 +1,13 @@
-from backend.findMeHome.resources.resources import AdapterApi, ModelApi
+from backend.findMeHome.resources.resources import signUpApi, ModelApi, signInApi,dogApi
 
 # baseUrl = 'http://127.0.0.1:5000'
 signUpURL = '/api/v0.1/user'
 modelURL = "/api/dog_model"
-
+signInURL = '/api/v0.1/userLogin'
+dogApiURL = '/api/v0.1/dogApi'
 
 def initialize_routes(api):
-    api.add_resource(AdapterApi, signUpURL)
+    api.add_resource(signUpApi, signUpURL)
     api.add_resource(ModelApi, modelURL)
+    api.add_resource(signInApi,signInURL)
+    api.add_resource(dogApi,dogApiURL)
