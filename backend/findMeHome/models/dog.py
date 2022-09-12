ad806c8e203679495edd38d2b115b01b6e8662fd
Rafay Qayyum <rafayqayyum786@gmail.com>
2022-09-13 03:03:37 +0500

    fixed Imports

--- backend/findMeHome/models/dog.py
+++ backend/findMeHome/models/dog.py
@@ -1,7 +1,6 @@
-from backend.findMeHome.models.Base import Base
+from backend.findMeHome.models.Base import Base,MAX_LENGTH
 from sqlalchemy import Column, String,Integer,ForeignKey
 from sqlalchemy.orm import relationship
-MAX_LENGTH=60
 class Dog(Base):
     __tablename__='dog'
     did=Column(Integer,primary_key=True,autoincrement=True)
