eb1fa9e59985f58e083582d294e21098448ff05e
GMY-Micky <mustafayousaf779@gmail.com>
2022-09-15 09:23:01 +0500

    change margin top

--- frontend/src/components/user/ProfileSetting.jsx
+++ frontend/src/components/user/ProfileSetting.jsx
@@ -37,20 +37,20 @@ const ProfileSetting = () =>{
 
                         </div>
                         <div className={"h-[1px] w-[100%] bg-[#70CF36] block"}></div>
-                        <div className={"flex justify-center items-center mt-10"}>
+                        <div className={"flex justify-center items-center mt-20"}>
                             <form onSubmit={submitHandle} className={"form max-w-[800px] w-[100%]"}>
                                 <div className={"flex w-[100%]"}>
                                     <div className={"flex flex-col w-[100%]"}>
                                         <div className={"h-[90px] mb-5"}>
-                                            <label className={"text-[15px] text-[#3E665C] font-bold"}>Firstname</label><br/>
+                                            <label className={"text-[16px] text-[#3E665C] font-bold"}>Firstname</label><br/>
                                             <input className={"bg-transparent border-b border-[#3E665C] w-[100%] text-[#7F99A2] h-[50px] p-2 outline-0 focus:border-[#70CF36]"} type={"text"} value={firstname} onChange={(e)=>setFirstname(e.target.value)}/>
                                         </div>
                                         <div className={"h-[90px] mb-5"}>
-                                            <label className={"text-[15px] text-[#3E665C] font-bold"}>Username</label><br/>
+                                            <label className={"text-[16px] text-[#3E665C] font-bold"}>Username</label><br/>
                                             <input className={"bg-transparent border-b border-[#3E665C] w-[100%] text-[#7F99A2] h-[50px] p-2 outline-0 focus:border-[#70CF36]"} type={"text"} value={username} onChange={(e)=>setUsername(e.target.value)}/>
                                         </div>
                                         <div className="h-[90px] mb-5">
-                                            <label className={"text-[15px] text-[#3E665C] font-bold"}>Country</label>
+                                            <label className={"text-[16px] text-[#3E665C] font-bold"}>Country</label>
                                             <br/>
                                             <CountryDropdown
                                                 required
@@ -61,7 +61,7 @@ const ProfileSetting = () =>{
 
                                         </div>
                                         <div className={"h-[90px] mb-5"}>
-                                            <label className={"text-[15px] text-[#3E665C] font-bold"}>City</label><br/>
+                                            <label className={"text-[16px] text-[#3E665C] font-bold"}>City</label><br/>
                                         <select
                                             onChange={(e) => e.target.value === "Select City" ? setCity(null) : setCity(e.target.value)}
                                             required={true} name="city"
@@ -72,22 +72,22 @@ const ProfileSetting = () =>{
                                         </select>
                                         </div>
                                         <div className={"h-[90px] mb-5"}>
-                                            <label className={"text-[15px] text-[#3E665C] font-bold"}>Contact</label><br/>
+                                            <label className={"text-[16px] text-[#3E665C] font-bold"}>Contact</label><br/>
                                             <input className={"bg-transparent border-b border-[#3E665C] w-[100%] text-[#7F99A2] h-[50px] p-2 outline-0 focus:border-[#70CF36]"} type={"text"} value={contact} onChange={(e)=>setContact(e.target.value)}/>
                                         </div>
 
                                     </div>
                                     <div className={"flex flex-col ml-10 w-[100%]"}>
                                         <div className={"h-[90px] mb-5"}>
-                                            <label className={"text-[15px] text-[#3E665C] font-bold"}>Lastname</label><br/>
+                                            <label className={"text-[16px] text-[#3E665C] font-bold"}>Lastname</label><br/>
                                             <input className={"bg-transparent border-b border-[#3E665C] w-[100%] text-[#7F99A2] h-[50px] p-2 outline-0 focus:border-[#70CF36]"} type={"text"} value={lastname} onChange={(e)=>setLastname(e.target.value)}/>
                                         </div>
                                         <div className={"h-[90px] mb-5"}>
-                                            <label className={"text-[15px] text-[#3E665C] font-bold"}>Email</label><br/>
+                                            <label className={"text-[16px] text-[#3E665C] font-bold"}>Email</label><br/>
                                             <input className={"bg-transparent border-b border-[#3E665C] w-[100%] text-[#7F99A2] h-[50px] p-2 outline-0 focus:border-[#70CF36]"} type={"text"} value={email} onChange={(e)=>setEmail(e.target.value)}/>
                                         </div>
                                         <div className={"h-[90px] mb-5"}>
-                                            <label className={"text-[15px] text-[#3E665C] font-bold"}>State</label><br/>
+                                            <label className={"text-[16px] text-[#3E665C] font-bold"}>State</label><br/>
                                         <select
                                             onChange={(e) => onStateChange(e.target.value, setState, setCity, setCities)}
                                             required={true} name="state"
@@ -98,7 +98,7 @@ const ProfileSetting = () =>{
                                         </select>
                                         </div>
                                         <div className={"h-[90px] mb-5"}>
-                                            <label className={"text-[15px] text-[#3E665C] font-bold"}>Address</label><br/>
+                                            <label className={"text-[16px] text-[#3E665C] font-bold"}>Address</label><br/>
                                             <input className={"bg-transparent border-b border-[#3E665C] w-[100%] text-[#7F99A2] h-[50px] p-2 outline-0 focus:border-[#70CF36]"} type={"text"} value={address} onChange={(e)=>setAddress(e.target.value)}/>
                                         </div>
                                         <button    className={"rounded-[21px] mt-8 text-white text-[13px] font-[600] bg-[#3E665C] w-full h-[40px]"} type={"submit"}>Update</button>
