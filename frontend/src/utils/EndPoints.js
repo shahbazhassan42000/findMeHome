035e079886dbf316a0a34968e333d53f4ff98b7d
shahbazhassan42000 <shahbazhassan42000@gmail.com>
2023-04-14 21:19:56 +0500

    added backedend api and imgBBkey to .env

--- frontend/src/utils/EndPoints.js
+++ frontend/src/utils/EndPoints.js
@@ -1,9 +1,8 @@
 // export const backendURL = "http://192.168.43.165:4000"; //RAFAY
 // export const backendURL = "http://192.168.159.165:4000"; //RAFAY2
 // export const backendURL = "http://192.168.8.100:4000"; //PUCIT
-export const backendURL = "http://localhost:4000";
+//export const backendURL = "http://localhost:4000";
 export const imgBBURL = "https://api.imgbb.com/1/upload";
-export const imgBBApiKey = "4d0eff80cd1cea3d5f1f524ac3a0808a";
 export const api2PdfURL="https://v2018.api2pdf.com/chrome/html";
 export const statesURL="https://countriesnow.space/api/v0.1/countries/states";
 export const citiesURL="https://countriesnow.space/api/v0.1/countries/state/cities";
